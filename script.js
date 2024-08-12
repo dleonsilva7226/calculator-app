f084eafc88a1583d4452dba636f737a6be074abf
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-08-11 21:45:09 -0400

    Add Invalid Operation Case

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ const buttons = document.querySelectorAll(".calc-button");
 buttons.forEach(function (button){
     button.addEventListener("click", function() {
         let inputValue = document.getElementById("input-box");
-        if ((currentOperation.length == 0 || currentOperation.length == 2)) {
+        if (button.innerText === "C") {
+            inputValue.value = "";
+            currentOperation = [];
+        } else if ((currentOperation.length == 0 || currentOperation.length == 2)) {
             if (numbersArr.includes(button.innerText)) {
                 currentOperation.push(button.innerText);
                 inputValue.value =  button.innerText;
@@ -21,18 +24,22 @@ buttons.forEach(function (button){
             currentOperation.push(button.innerText);
             // inputValue.value =  currentOperation[0];
         } else if  (currentOperation.length == 1 && numbersArr.includes(button.innerText)) {
-            currentOperation[0] = currentOperation[0] + button.innerText;
-            inputValue.value =  currentOperation[0];
+            if (currentOperation[0].length < 10) {
+                currentOperation[0] = currentOperation[0] + button.innerText;
+                inputValue.value =  currentOperation[0];
+            }
         } else if (currentOperation.length == 3 && numbersArr.includes(button.innerText)) {
-            currentOperation[2] = currentOperation[2] + button.innerText;
-            inputValue.value = currentOperation[2];
+            if (currentOperation[2].length < 10) {
+                currentOperation[2] = currentOperation[2] + button.innerText;
+                inputValue.value = currentOperation[2];
+            }
         } else if (currentOperation.length == 3 && button.innerText === "=") {
-            inputValue.value = operate(currentOperation[1], parseFloat(currentOperation[0]), parseFloat(currentOperation[2])).toFixed(5);
+            inputValue.value = operate(currentOperation[1], parseFloat(currentOperation[0]), parseFloat(currentOperation[2]));
+            if (inputValue.value !== "Invalid!") {
+                inputValue.value = inputValue.value.toFixed(5);
+            }
             currentOperation = [];
             currentOperation.push(inputValue.value);
-        } else if (button.innerText === "C") {
-            inputValue.value = "";
-            currentOperation = [];
         } else if (button.innerText === "+/-") {
             if (currentOperation.length == 1) {
                 inputValue.value = (parseFloat(currentOperation[0]) * -1).toString();
@@ -72,7 +79,7 @@ function operate (operator, numOne, numTwo) {
     } else if (operator === "/") {
         return divide(numOne, numTwo);
     } else {
-        return "Invalid Operation";
+        return "Invalid!";
     }
 }
 
@@ -88,5 +95,6 @@ function multiply (numOne, numTwo) {
     return numOne * numTwo;
 }
 function divide (numOne, numTwo) {
+    if (numTwo == 0) {return "Invalid!";}
     return numOne / numTwo;
 }
