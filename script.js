d8967c13ac2556d172496db84580312ed871b919
Daniel Leon Silva <dleonsilva7226@gmail.com>
2024-08-11 21:03:07 -0400

    Implement Functionalities of Calculator

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let numOne = 1;
 let numTwo = 9;
 // let operator = "+";
-const numbersArr = ["0","1","2","3","4","5","6","7","8","9"];
-const coreOperatorsArr = ["/", "*", "%", "+", "-"];
+const numbersArr = ["0","1","2","3","4","5","6","7","8","9", "."];
+const coreOperatorsArr = ["/", "*", "+", "-"];
 
 let currentOperation = [];
 
@@ -12,26 +12,52 @@ const buttons = document.querySelectorAll(".calc-button");
 buttons.forEach(function (button){
     button.addEventListener("click", function() {
         let inputValue = document.getElementById("input-box");
-        if ((currentOperation.length == 0 || currentOperation.length == 2) && numbersArr.includes(button.innerText)) {
-            currentOperation.push(button.innerText);
-            inputValue.value =  inputValue.value + button.innerText;
+        if ((currentOperation.length == 0 || currentOperation.length == 2)) {
+            if (numbersArr.includes(button.innerText)) {
+                currentOperation.push(button.innerText);
+                inputValue.value =  button.innerText;
+            } 
         } else if (currentOperation.length == 1 && coreOperatorsArr.includes(button.innerText)) {
             currentOperation.push(button.innerText);
-            inputValue.value =  inputValue.value + button.innerText;
+            // inputValue.value =  currentOperation[0];
         } else if  (currentOperation.length == 1 && numbersArr.includes(button.innerText)) {
             currentOperation[0] = currentOperation[0] + button.innerText;
-            inputValue.value =  inputValue.value + button.innerText;
+            inputValue.value =  currentOperation[0];
         } else if (currentOperation.length == 3 && numbersArr.includes(button.innerText)) {
             currentOperation[2] = currentOperation[2] + button.innerText;
-            inputValue.value =  inputValue.value + button.innerText;
+            inputValue.value = currentOperation[2];
         } else if (currentOperation.length == 3 && button.innerText === "=") {
-            inputValue.value = operate(currentOperation[1], parseInt(currentOperation[0]), parseInt(currentOperation[2]));
+            inputValue.value = operate(currentOperation[1], parseFloat(currentOperation[0]), parseFloat(currentOperation[2])).toFixed(5);
             currentOperation = [];
+            currentOperation.push(inputValue.value);
         } else if (button.innerText === "C") {
             inputValue.value = "";
             currentOperation = [];
+        } else if (button.innerText === "+/-") {
+            if (currentOperation.length == 1) {
+                inputValue.value = (parseFloat(currentOperation[0]) * -1).toString();
+                currentOperation[0] = inputValue.value;
+            } else if (currentOperation.length == 3) {
+                inputValue.value = (parseFloat(currentOperation[2]) * -1).toString();
+                currentOperation[2] = inputValue.value;
+            }
+        } else if (button.innerText === "%") {
+            if (currentOperation.length == 1) {
+                inputValue.value = (parseFloat(currentOperation[0]) / 100).toString();
+                currentOperation[0] = inputValue.value;
+            } else if (currentOperation.length == 3) {
+                inputValue.value = (parseFloat(currentOperation[2]) / 100).toString();
+                currentOperation[2] = inputValue.value;
+            }
+        } 
+        else if (currentOperation.length == 2 && coreOperatorsArr.includes(button.innerText)) {
+            currentOperation[1] = button.innerText;
+        } else if (button.innerText === ".") {
+            if (!inputValue.value.includes(".")) {
+                inputValue.value = inputValue.value + ".";
+            }
         }
-        console.log(currentOperation.toString());
+        // console.log(currentOperation.toString());
     });
 });
 
@@ -44,7 +70,7 @@ function operate (operator, numOne, numTwo) {
     } else if (operator === "*") {
         return multiply(numOne, numTwo);
     } else if (operator === "/") {
-        return divide(numOne, numTwo).toFixed(3);
+        return divide(numOne, numTwo);
     } else {
         return "Invalid Operation";
     }
@@ -64,8 +90,3 @@ function multiply (numOne, numTwo) {
 function divide (numOne, numTwo) {
     return numOne / numTwo;
 }
-
-
-
-
-console.log(operate("/", numOne, numTwo));
